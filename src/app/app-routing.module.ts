c15ae066ea3c4d64efbadeb58fc4d187888bfcf0
mjmendez <majomendez88mena@gmail.com>
2020-05-25 15:47:41 -0600

    Nuevas funcionalidades de clases ion como css, visto  en doc: https://ionicframework.com/docs/layout/css-utilities

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,21 +2,30 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+	{
+		path: '',
+		redirectTo: 'inicio',
+		pathMatch: 'full',
+	},
+	{
+		path: 'inicio',
+		loadChildren: () =>
+			import('./pages/inicio/inicio.module').then((m) => m.InicioPageModule),
+	},
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: 'alert',
+    loadChildren: () => import('./pages/alert/alert.module').then( m => m.AlertPageModule)
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'action-sheet',
+    loadChildren: () => import('./pages/action-sheet/action-sheet.module').then( m => m.ActionSheetPageModule)
   },
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+	imports: [
+		RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+	],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
